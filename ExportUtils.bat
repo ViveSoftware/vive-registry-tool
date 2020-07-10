f33afa338e5b17276c5edccd9c044c9833e8f3f1
Cheng Han Chiang <chenghan_chiang@htc.com>
2020-07-10 18:12:43 +0800

    Prevent potential conflicts with VIU

--- ExportUtils.bat
+++ ExportUtils.bat
@@ -15,19 +15,13 @@ if exist %LOG_FILE_PATH% (
 		rmdir /s /q %OUTPUT_PATH%
 	)
 	
-	robocopy /np /njh /njs "Assets/%ROOT_FOLDER_NAME%/Editor/Resources" "%OUTPUT_PATH%/Editor/Resources" "LICENSE.txt*"
-	robocopy /np /njh /njs "Assets/%ROOT_FOLDER_NAME%/Editor/Resources" "%OUTPUT_PATH%/Editor/Resources" "RegistrySettings.json*"
-	robocopy /np /njh /njs "Assets/%ROOT_FOLDER_NAME%/Editor/Scripts/Configs" "%OUTPUT_PATH%/Editor/Scripts/Configs" "RegistrySettings.cs*"
-	robocopy /np /njh /njs "Assets/%ROOT_FOLDER_NAME%/Editor/Scripts/Utils" "%OUTPUT_PATH%/Editor/Scripts/Utils"
-	robocopy /np /njh /njs "Assets/%ROOT_FOLDER_NAME%/Editor/Scripts" "%OUTPUT_PATH%/Editor/Scripts" "Configs.meta"
-	robocopy /np /njh /njs "Assets/%ROOT_FOLDER_NAME%/Editor/Scripts" "%OUTPUT_PATH%/Editor/Scripts" "Utils.meta"
-	robocopy /np /njh /njs "Assets/%ROOT_FOLDER_NAME%/Editor/Scripts" "%OUTPUT_PATH%/Editor/Scripts" "HTC.UPMRegistryTool.Editor.asmdef*"
-	robocopy /np /njh /njs "Assets/%ROOT_FOLDER_NAME%/Editor" "%OUTPUT_PATH%/Editor" "Resources.meta"
-	robocopy /np /njh /njs "Assets/%ROOT_FOLDER_NAME%/Editor" "%OUTPUT_PATH%/Editor" "Scripts.meta"
-	robocopy /np /njh /njs /s "Assets/%ROOT_FOLDER_NAME%/Plugins" "%OUTPUT_PATH%/Plugins"
-	robocopy /np /njh /njs "Assets/%ROOT_FOLDER_NAME%" "%OUTPUT_PATH%" "VERSION*"
-	robocopy /np /njh /njs "Assets/%ROOT_FOLDER_NAME%" "%OUTPUT_PATH%" "Editor.meta"
-	robocopy /np /njh /njs "Assets/%ROOT_FOLDER_NAME%" "%OUTPUT_PATH%" "Plugins.meta"
+	robocopy /np /njh /njs "Assets/%ROOT_FOLDER_NAME%/Editor/Resources" "%OUTPUT_PATH%/Editor/Resources" "LICENSE.txt"
+	robocopy /np /njh /njs "Assets/%ROOT_FOLDER_NAME%/Editor/Resources" "%OUTPUT_PATH%/Editor/Resources" "RegistrySettings.json"
+	robocopy /np /njh /njs "Assets/%ROOT_FOLDER_NAME%/Editor/Scripts/Configs" "%OUTPUT_PATH%/Editor/Scripts/Configs" "RegistrySettings.cs"
+	robocopy /np /njh /njs "Assets/%ROOT_FOLDER_NAME%/Editor/Scripts/Utils" "%OUTPUT_PATH%/Editor/Scripts/Utils" /xf "*.meta"
+	robocopy /np /njh /njs "Assets/%ROOT_FOLDER_NAME%/Editor/Scripts" "%OUTPUT_PATH%/Editor/Scripts" "HTC.UPMRegistryTool.Editor.asmdef"
+	robocopy /np /njh /njs /s "Assets/%ROOT_FOLDER_NAME%/Plugins" "%OUTPUT_PATH%/Plugins" /xf "*.meta"
+	robocopy /np /njh /njs "Assets/%ROOT_FOLDER_NAME%" "%OUTPUT_PATH%" "VERSION"
 )
 
 ENDLOCAL
